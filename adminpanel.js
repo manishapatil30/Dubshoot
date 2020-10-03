240d3eacba0921917dee895315cdc23e63a5c397
manishapatil30 <manisha@mtouchlabs.com>
2020-10-03 17:22:17 +0530

    master

--- adminpanel.js
+++ adminpanel.js
@@ -1,4 +1,4 @@
-var app = angular.module("DemoApp", ['ng','ngRoute', 'ngCookies']);
+var app = angular.module("DemoApp", ['ng','ngRoute', 'ngCookies', 'angularSpinkit']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
